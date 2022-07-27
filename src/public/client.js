d91f17208937e19224fc24ad97c085dc6457b6f5
robertpfaff <42342840+robertpfaff@users.noreply.github.com>
2022-07-27 10:26:15 -0400

    First working version
    Need to add camera filter and make pretty!

--- src/public/client.js
+++ src/public/client.js
@@ -15,20 +15,73 @@ let store = {
 }
 
 // add our markup to the page
-
 const updateStore = (store, newState) => {
+    console.log("Store in UpdateStore:")
+    console.log(store)
+    console.log("Store in UpdateStore:")
+    console.log(store)
     store = Object.assign(store, newState)
+    console.log("Store after Assign:")
+    console.log(store)
     console.log("Store updated")
     render(root, store)
-}
+};
 
 const render = async (root, state) => {
     root.innerHTML = App(state)
+    console.log("APP: state")
+    console.log(App(state))
+    console.log("ROOT")
+    console.log(App(root.innerHTML))
 }
 
 // Helper function/code to capitalize words as neccessary.
 const capitalize = ([first, ...rest], lowerRest = false) =>
-first.toUpperCase() + (lowerRest ? rest.join('').toLowerCase() : est.join(''));
+first.toUpperCase() + (lowerRest ? rest.join('').toLowerCase() : rest.join(''));
+
+const getRoverPhotos = async (store, fn) => {
+
+    const name = document.getElementById('name').value;
+    console.log(name)
+    const rover = document.getElementById('rover').value;
+    console.log(rover)
+    const camera = document.getElementById('camera').value;
+    console.log(camera)
+
+    const userInfo = new Object();
+    userInfo.name = name
+    userInfo.rover = rover
+    userInfo.camera = camera
+
+    console.log("User Info Object:")
+    console.log("Parsing into JSON string...")
+    const body = JSON.stringify(userInfo)
+    console.log(body)
+    console.log(body.length)
+
+    const options = {
+      method: "POST",
+      headers: {'Content-Type': "application/json"},
+      body: body
+    };
+
+    console.log("CLIENT SIDE RESPONSE: FETCH IMAGE DATA")
+    const response = await fetch('/results', options)
+    console.log("Okay? ", response.ok)
+    console.log("Unexpected end of input?")
+    console.log(response)
+
+    const json = await response.json();
+    console.log(json)
+
+    if (response.ok === true) {
+        const newState = json
+        updateStore(store, newState)
+        console.log("Response OK. New Store: ", store)
+        return store
+    } else {
+        throw new Error()
+}};
 
 // Render content based on user input chosenRover.
 // if chosenRover is defined, run function to create slideshow.
@@ -39,31 +92,30 @@ first.toUpperCase() + (lowerRest ? rest.join('').toLowerCase() : est.join(''));
 
 const App = (state) => {
     let { store } = state
-    console.log("What is APP now?")
-    console.log(App)
 
+    // Store no longer exists. Absorbed by state.
     console.log("What is state now?")
-    console.log(App)
-
-    console.log("Does store exist inside App now?")
-    console.log(store)
+    console.log(state)
 
     // Makes and merges rover and photo info objects for cards.
     // Some images are duplicates, but urls are differet.
 
     const showRoverPhotos = (state) => {
-        const imageURLs = state.latest_photos.map(photo => photo.img_src)
-        const imageObjects = state.latest_photos.map(photo => photo.img_src = new Image(600,600))
-        const imageDates = state.latest_photos.map(photo => photo.earth_date)
-        const cameraNames = state.latest_photos.map(photo => photo.camera.full_name)
-        const roversNames = state.latest_photos.map(photo => photo.rover.name)
-        const roversStatus = state.latest_photos.map(photo => photo.rover.status)
-        const roversLaunchDates = state.latest_photos.map(photo => photo.rover.launch_date)
-        const roversLandDates = state.latest_photos.map(photo => photo.rover.landing_date)
+        const imageURLs = state.gallery.latest_photos.map(photo => photo.img_src)
+        const imageObjects = state.gallery.latest_photos.map(photo => photo.img_src = new Image(600,600))
+        const imageDates = state.gallery.latest_photos.map(photo => photo.earth_date)
+        const cameraNames = state.gallery.latest_photos.map(photo => photo.camera.full_name)
+        const roversNames = state.gallery.latest_photos.map(photo => photo.rover.name)
+        const roversStatus = state.gallery.latest_photos.map(photo => photo.rover.status)
+        const roversLaunchDates = state.gallery.latest_photos.map(photo => photo.rover.launch_date)
+        const roversLandDates = state.gallery.latest_photos.map(photo => photo.rover.landing_date)
 
         const roverPhotoInfo = (imageURLs, imageObjects, imageDates, cameraNames, roversNames, roversStatus, roversLaunchDates, roversLandDates) => imageURLs.map((imageURL, i) => ({imageURL, imageObject: imageObjects[i], imageDate: imageDates[i], cameraName: cameraNames[i], roverName: roversNames[i],
             roverStatus: roversStatus[i], roverLaunchDate: roversLaunchDates[i], roverLandDate: roversLandDates[i]
     }));
+
+    console.log("Results: Rover Photo Info")
+    console.log(roverPhotoInfo);
 
     console.log("showRoverPhotos mapped new arrays")
     photoInfo = roverPhotoInfo(imageURLs, imageObjects, imageDates, cameraNames, roversNames, roversStatus, roversLaunchDates, roversLandDates)
@@ -82,10 +134,11 @@ const App = (state) => {
     <br>
     <br>
     <p><span>Rover Name:</span> ${item.roverName}</p>
-    <p><span>Rover Status:</span> ${item.roverStatus}</p>
+    <p><span>Rover Status:</span> ${capitalize(item.roverStatus)}</p>
     <p><span>Launch Date:</span> ${item.roverLaunchDate}</p>
     <p><span>Landing Date:</span> ${item.roverLandDate}</p>
     <p><span>Image Date:</span> ${item.imageDate}</p>
+    <p><span>Image Date:</span> ${item.cameraName}</p>
     <p></p>
     <br>
     <hr/>
@@ -93,17 +146,17 @@ const App = (state) => {
     </div>`).slice(0, 50).join(""))
 }
 
-console.log("showRoverPhotos called inside html. returns user welcome.")
-
-if (state.rover != undefined || null || '' || 'none') {
-    const rover = capitalize(rover)
+// Image maps not defined. Only state.
+if (state.gallery.latest_photos[0].rover.name != undefined || null || '' || 'none') {
+    console.log("showRoverPhotos called inside html. returns user welcome.")
+    const chosenRover = capitalize(state.gallery.latest_photos[0].rover.name)
     return (`
     <header>
-    <h2 class="main-title">Welcome to the ${rover} Rover's Gallery</h2>
+    <h2 class="main-title">Welcome to the ${chosenRover} Rover's Gallery</h2>
     </header>
     <div class="main">
     <hr />
-    <p><h4>Greetings, Earthling! Mission Completed.</p>
+    <p><h4>Greetings, Earthling! Welcome to the Red Planet.</p>
     <br>
     <br>
     <div>${showRoverPhotos(state)}
@@ -116,40 +169,9 @@ if (state.rover != undefined || null || '' || 'none') {
     `)
 };
 
-// Closure ends
+// APP Closure ends
 // END OF APP FUNCTION
-
 console.log("Closure ends here.")
-
 };
 
-// Single async higher-order/callback function
-// Retrieves image data and update store/new state
-// getRoverPhotos is Higher Order function.
-// RoverPhotos as callback function.
-
-const getRoverPhotos = async (state, fn) => {
-
-    console.log("CIENT SIDE API RUNNING.")
-
-    const response = await fetch("http://localhost:3000/userinput", {
-        method: "POST",
-        body: body,
-        headers: {'Content-Type': 'application/JSON' }
-});
-
-const jsonImageData = await response.json()
-.then(jsonImageData => console.log(jsonImageData))
-
-const ImageData = jsonImageData.gallery.latest_photos.map(photo => {
-    return photo.img_src;
-
-});
-
-    const newState = jsonImageData.gallery
-    updateStore(store, newState)
-    return ImageData
-}
-
-getRoverPhotos(store, updateStore)
 
--- src/server/index.js
+++ src/server/index.js
@@ -1,36 +1,40 @@
-
 const express = require('express');
-const bodyParser = require('body-parser');
-const path = require('path')
+const path = require('path');
 const cors = require('cors')
 const fetch = require('node-fetch');
 const app = express();
 
 app.use('/', express.static(path.join(__dirname, '../public')));
+app.use(express.json());
+app.use(express.json( { limit: '1mg' }));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors({ origin: '*' }));
 
-app.use(cors("*"));
+// request is what getRoverPhotos sends to server
+// response are (we hope) image URLs from NASA
 
-app.post('/userinput', async (request, response) => {
+app.post('/results', async (request, response) => {
 
+  console.log("SERVER SIDE: Rover");
   const rover = request.body.rover
+  console.log(rover)
 
   try {
     const gallery = await fetch(`https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/latest_photos?api_key=DH46IQlx0gMyXPmLAgkxXDSPo2OrbIjPs8OJLj6L`)
-    .then(response => response.json()) // send json to client
+    .then(response => response.json()) // send json back to client
     .then(gallery => response.send({gallery})) // send gallery to browser (so I can see it)
   } catch (err) {
-    console.log(`ERROR: ${response.status}`, err);
+    console.log(`RESPONSE STATUS: ${response.status}`, err);
   }
-  return response.json
+  console.log("GALLERY:")
+  console.log(response.json())
+  return response.json()
 
 });
 
 const port = 3000;
 
 app.listen(port, () => {
-  console.log(`Server running on port${port}`);
-
+  console.log(`Server running on port ${port}`);
 
 });
